c790e8dbb7577b6bdca3058c90171538cdfa2343
Brandon Kessler <brandon.j.kessler@outlook.com>
2024-08-19 14:06:47 -0600

    func: removes unused write-customeventlog

--- Compare-Csv.ps1
+++ Compare-Csv.ps1
@@ -20,19 +20,6 @@ Param(
 
 begin{
 
-    Function Write-CustomEventLog{
-        [cmdletbinding()]
-        param(
-            [string]$Message,
-            [int]$ID = 0001,
-            [int]$Category = 0,
-            [string]$EventSource = "Custom Event Log"
-        )
-        if ([System.Diagnostics.EventLog]::Exists('Application') -eq $False -or [System.Diagnostics.EventLog]::SourceExists($EventSource) -eq $False){
-            New-EventLog -LogName Application -Source $EventSource  | Out-Null
-        }
-        Write-EventLog -LogName Application -Source $EventSource -EntryType Information -EventId $ID -Message $Message -Category $Category
-    }
 
     $timestamp = Get-Date -Format yyyy-MM-dd_HH-mm
 
